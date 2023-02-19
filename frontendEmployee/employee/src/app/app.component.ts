2e19db2860d5a4645ecda81e5871ab10220ddf10
NisalJayathilaka <nisal7075399@gmail.com>
2023-02-19 19:07:44 +0530

    Front end upto addEmployee

--- frontendEmployee/employee/src/app/app.component.ts
+++ frontendEmployee/employee/src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'employee';
+export class AppComponent implements OnInit{
+  employees: Employee[] = [];
+
+   constructor(private employeeService : EmployeeService){}
+
+   ngOnInit(): void {
+     this.getEmployees()
+   }
+
+   public getEmployees():void{
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+        console.log(this.employees);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+   }
+
+   public onAddEmployee(addForm:NgForm):void{
+    document.getElementById('add-employee-form')!.click();
+    this.employeeService.addEmployee(addForm.value).subscribe(
+      (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+        addForm.reset();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+        addForm.reset();
+      }
+    );
+   }
+
+
+   public onOpenModal(employee:any,mode: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addEmployeeModal');
+    }
+    if (mode === 'edit') { 
+      button.setAttribute('data-target', '#updateEmployeeModal');
+    }
+    if (mode === 'delete') {
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
+    container?.appendChild(button)
+    button.click();
+  }
 }
---
+++ frontendEmployee/employee/src/app/employee.ts
@@ -0,0 +1,9 @@
+export interface Employee {
+  employeeId: number;
+  employeeName: string;
+  employeeEmail: string;
+  employeeJobTitle: string;
+  employeePhone: string;
+  employeeImageUrl: string;
+  employeeCode: string;
+}
